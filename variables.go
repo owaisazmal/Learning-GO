50c241fb31243f9cf83d815e2a87fa91bf1220d5
Owais <owais.khan.244@my.csun.edu>
2023-09-04 14:52:27 -0700

    2nd commit

---
+++ variables.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+//package level declaration
+
+var x float32 = 4
+
+func main() { // 3 Ways to declare
+
+	// var i float32 = 1 //i
+	// fmt.Println(i)
+
+	// var j int //ii
+	// j = 2
+	// j = 69
+	// fmt.Println(j)
+	// //fmt.Println(423)
+
+	// k := 3 //it can figure the data type by itself				iii
+	// fmt.Println(k)
+
+	// Formating String
+	fmt.Printf("%v, %T", x, x) // %v->the var, %T->type
+
+}
