57fb30ec1cf20ce24cc57be716dce02d2705ff95
Owais <owais.khan.244@my.csun.edu>
2023-09-04 14:16:05 -0700

    Initial Commit

--- user-input.go
+++ user-input.go
@@ -11,5 +11,5 @@ func main() {
 	fmt.Println("Enter Last Name: ")
 	fmt.Scan(&lName)
 
-	fmt.Print("Full Name is: " + fName + " " + lName)
+	fmt.Print("Full Name is: " + fName + " " + lName) //Commit
 }
