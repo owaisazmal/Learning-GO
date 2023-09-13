3ad09808e0dcac0d2f771b09192984cb0f968d02
Owais <owais.khan.244@my.csun.edu>
2023-09-12 22:42:34 -0700

    if,math

---
+++ mathfunc.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt"
+		"math"
+)
+func main(){
+	myNum := 0.123
+	if math.Abs(myNum / math.Pow(math.Sqrt(myNum), 2) - 1) < 0.001 {
+		fmt.Println("These are the same")
+	} else{
+		fmt.Print("these are different!")
+	}
+}
---
+++ switch.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	switch 1{
+	case 1, 5, 10:
+		fmt.Println("one, five or 10")
+	case 2:
+		fmt.Println("two")
+	default: 
+		fmt.Println("none one or two")
+	}
+}
