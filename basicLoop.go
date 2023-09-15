767d8eba76806cdd2e7dfc747580c1e22e8e9d3f
Owais <owais.khan.244@my.csun.edu>
2023-09-14 21:29:54 -0700

    loopCommmit

--- basicLoop.go
+++ basicLoop.go
@@ -6,4 +6,9 @@ func main(){
 	for i, j := 0, 0; i < 5; i, j = i+1, j+1{		//can't use i++ and j++ together
 		fmt.Println(i, j)
 	}
+	j := 0							//cleaner way
+	for j < 5 {
+		fmt.Print(j)
+		j++
+	}
 }
