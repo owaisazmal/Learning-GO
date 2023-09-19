5b2c373f9aa9c0a654545b1bb51e254d3e955716
Owais <owais.khan.244@my.csun.edu>
2023-09-18 21:15:24 -0700

    method invocation

---
+++ methods.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	g := greeter{
+		greeting : "Hello",
+		name: "Go",
+	}
+	g.greet()		//method invocation
+}
+type greeter struct{		value creator
+	greeting string
+	name string
+}
+// type counter int{
+	
+// }	
+func (g greeter)greet(){		//value reciever
+	fmt.Println(g.greeting, g.name)
+
+func (g *greeter)greet(){		//pointer reciever
+	fmt.Println(g.greeting, g.name)
+}
