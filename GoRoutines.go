47510495d12fcfb4895114028abd04e9cc3b2a30
Owais <owais.khan.244@my.csun.edu>
2023-09-22 16:10:16 -0700

    GoRoutines

--- GoRoutines.go
+++ GoRoutines.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+var wg = sync.WaitGroup{
+
+}
 func main() {
-	go sayHello()
+	var msg = "Hello"
+	go func (msg string)  {
+		fmt.Println(msg)
+	}(msg)
+	msg = "Goodbye"
+	//go sayHello()
+	time.Sleep(100 * time.Millisecond)			//bad practice
 }
 
-func sayHello(){
-	fmt.Println("Hello")
-}
+// func sayHello(){
+// 	fmt.Println("Hello")
+// }
