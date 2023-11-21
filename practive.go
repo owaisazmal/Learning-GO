5f24a1fd07410401481c3bc0c71d79e4d4f925fe
Owais <owais.khan.244@my.csun.edu>
2023-11-20 16:17:49 -0800

    new solutions

--- practive.go
+++ practive.go
@@ -1,34 +1,82 @@
 package main
 
+//****************Q1*******************
+
+// func Print(input string){
+// 	fmt.Println(input)
+// }
+
+//****************Q2*******************
+
+// import (
+//   "fmt"
+//   "math/rand"
+// )
+
+// func main() {
+
+//   randArr := intRandom1DArray(2)
+
+//     fmt.Println(randArr)
+// }
+
+// func intRandom1DArray(size int) ([]int) {
+
+//   res := make([]int, size)
+
+//     for i := 0; i < size; i++{
+
+//       res[i] = rand.Int()
+
+//     }
+
+//   return res
+// }
+
+//****************Q5*******************
+
+// import "fmt"
+
+// func main() {
+
+// 	name := promptForInput("What's your name Homie")
+// 	fmt.Println("Sup Homie ", name, "!")
+
+// }
+
+// func promptForInput(msg string) string {
+
+// 	fmt.Print(msg + ": ")
+// 	var input string
+// 	fmt.Scanln(&input)
+
+// 	return input
+
+// }
+
+//Citation
+//https://www.w3schools.com/go/go_input.php
+
+
+//****************Q6*******************
+
+//NOTE: ONLY WORKS IN ECLIPSE BECAUSE OF GOLANG SPECIFICATIONS
+//go get -u github.com/mohae/deepcopy
+
 import (
 	"fmt"
-	"math/rand"
-	"time"
-)
-
-func main() {
-
-}
-
-func Print(input string){
-	fmt.Println(input)
-}
-
-// func Print(input int){
-// 	fmt.Println(input)
-// }
-
-// func Print(input double){
-// 	fmt.Println(input)
-// }
-
-func intRandom1DArray(size int) []int {
-	rand.Seed(time.Now().UnixNano())
-	array := make([]int, size)
-
-	for i := 0; i < size; i++ {
-		array[i] = rand.Intn(100) // Adjust 100 to the desired range for random integers
-	}
-
-	return array
-}
+	"github.com/pkg/go-stream" 
+  )
+  
+  func main() {
+	require (
+	  github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 
+	  github.com/pkg/go-stream v1.0.0
+	)
+	orderedArray(10)
+  }
+  
+  func orderedArray(size int) []int {
+	return gostream.IntStream(0, size).ToArray()
+  }
+  
